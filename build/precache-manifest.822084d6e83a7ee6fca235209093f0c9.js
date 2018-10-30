15b83f8ceb85eff7f8226543324a5c9da5a73344
LJEsp <esparragoleandroj@gmail.com>
2018-10-30 12:24:13 +0800

    Progress

---
+++ build/precache-manifest.822084d6e83a7ee6fca235209093f0c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "11adeb05b320643f4668",
+    "url": "/static/js/main.11adeb05.chunk.js"
+  },
+  {
+    "revision": "eb8cf8ba9e6e56db3e7e",
+    "url": "/static/js/1.eb8cf8ba.chunk.js"
+  },
+  {
+    "revision": "11adeb05b320643f4668",
+    "url": "/static/css/main.a2cbd961.chunk.css"
+  },
+  {
+    "revision": "52c3f12707217c0c48461a4b8a2feb64",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/ping-item/ping-item.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import Ping from "ping.js";
+// import webping from "web-pingjs";
+
+class PingItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.p = new Ping();
+
+    this.state = {
+      pingState: "-",
+      pingData: "-"
+    };
+  }
+
+  componentDidMount() {
+    if (!this.props.useUrl) {
+      this.p.ping(`http://www.${this.props.domain}`, (err, data) => {
+        if (err) {
+          return this.setState({ pingState: "DOWN", pingData: "" });
+        }
+
+        this.setState({ pingState: "UP", pingData: data });
+      });
+    } else {
+      this.p.ping(this.props.url, (err, data) => {
+        if (err) {
+          return this.setState({ pingState: "DOWN", pingData: "" });
+        }
+
+        this.setState({ pingState: "UP", pingData: data });
+      });
+    }
+
+    // webping(this.props.url)
+    //   .then(delta => {
+    //     console.log(
+    //       `${this.props.name}` + " ping time was " + String(delta) + " ms"
+    //     );
+    //   })
+    //   .catch(function(err) {
+    //     console.error("Could not ping remote URL", err);
+    //   });
+  }
+
+  render() {
+    return (
+      <div>
+        <a href={this.props.url} target="_blank" rel="noopener noreferrer">
+          {this.props.domain}
+        </a>{" "}
+        <span
+          style={
+            this.state.pingState === "DOWN"
+              ? { color: "red" }
+              : { color: "green" }
+          }
+        >
+          {this.state.pingState}{" "}
+        </span>
+        {this.state.pingData}
+      </div>
+    );
+  }
+}
+
+export default PingItem;
