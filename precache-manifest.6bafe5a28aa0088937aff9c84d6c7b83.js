a3d11eb1c7ad4c601a7e2007d6874632ed8147df
LJEsp <esparragoleandroj@gmail.com>
2018-10-30 12:33:33 +0800

    Updates

---
+++ precache-manifest.6bafe5a28aa0088937aff9c84d6c7b83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac376b6e078b2065427c",
+    "url": "/project-spi-ping-monitor/static/js/runtime~main.ac376b6e.js"
+  },
+  {
+    "revision": "193c1652b7ba04456377",
+    "url": "/project-spi-ping-monitor/static/js/main.193c1652.chunk.js"
+  },
+  {
+    "revision": "eb8cf8ba9e6e56db3e7e",
+    "url": "/project-spi-ping-monitor/static/js/1.eb8cf8ba.chunk.js"
+  },
+  {
+    "revision": "193c1652b7ba04456377",
+    "url": "/project-spi-ping-monitor/static/css/main.a2cbd961.chunk.css"
+  },
+  {
+    "revision": "277090c291b9100b4ceda95c283af3fb",
+    "url": "/project-spi-ping-monitor/index.html"
+  }
+];
